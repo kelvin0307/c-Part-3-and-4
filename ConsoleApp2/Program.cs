8665a92a06e4287fdd1ae139b72aac15c0917f00
kelvin lammerts van bueren <kelvinlvb2002@hotmail.com>
2023-03-08 12:31:29 +0100

    Part 3 opdr 28

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,16 +1,8 @@
 ﻿// Part 3 opdr 21
 
-int[] array = { 5, 1, 3, 4, 2 };
-PrintArrayInStars(array);
-
-void PrintArrayInStars(int[] array)
+string line;
+while ((line = Console.ReadLine()) != "")
 {
-    foreach (int i in array)
-    {
-        for(int j = 0; j < i; j++)
-        {
-            Console.Write("*");
-        }
-        Console.WriteLine(" ");
-    }
+    string[] words = line.Split(' ');
+    Console.WriteLine(words[words.Length - 1]);
 }
