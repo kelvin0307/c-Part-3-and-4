64a3e437bb8eb6a80b632d3d8948258576a0e519
kelvin lammerts van bueren <kelvinlvb2002@hotmail.com>
2023-03-08 11:56:52 +0100

    Part 3 opdr 16

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Part 3 opdr 16
+
+List<string> strings = new List<string>();
+
+strings.Add("First");
+strings.Add("Second");
+strings.Add("Third");
+
+// Remember, this is how you print all the items in a list
+strings.ForEach(Console.WriteLine);
+
+static void RemoveLast(List<string> strings)
+{
+    strings.RemoveAt(strings.Count - 1);
+}
+
+RemoveLast(strings);
+RemoveLast(strings);
+
+strings.ForEach(Console.WriteLine);
