6f6d2831cee80a4836ef333c1b68e52fa6a0722b
kelvin lammerts van bueren <kelvinlvb2002@hotmail.com>
2023-03-08 12:42:36 +0100

    Part 3 opdr 29

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,18 @@
-﻿// Part 3 opdr 21
-
-string line;
-while ((line = Console.ReadLine()) != "")
+﻿// Part 3 opdr 28
+string input;
+List<int> ages = new List<int>();
+while ((input = Console.ReadLine()) != "")
 {
-    string[] words = line.Split(' ');
-    Console.WriteLine(words[words.Length - 1]);
+    string[] parts = input.Split(',');
+    int age = int.Parse(parts[1].Trim());
+    ages.Add(age);
 }
+int maxAge = ages[0];
+for (int i = 1; i < ages.Count; i++)
+{
+    if (ages[i] > maxAge)
+    {
+        maxAge = ages[i];
+    }
+}
+Console.WriteLine("The oldest person is {0} years old.", maxAge);
