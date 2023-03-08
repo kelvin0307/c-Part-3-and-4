fd7f695cfbc61092ad6524d17329f97bc07ec3c3
kelvin lammerts van bueren <kelvinlvb2002@hotmail.com>
2023-03-08 18:54:32 +0100

    Part 4 opdr 4

---
+++ ConsoleApp2/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Room
+    {
+        private string code;
+        private int seats;
+
+        public Room(string classCode, int numberOfSeats)
+        {
+            code = classCode;
+            seats = numberOfSeats;
+        }
+    }
+}
