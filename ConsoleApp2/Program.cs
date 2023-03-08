435d9a3a9701181c440df09fcd1b9d82c2b3b5d3
kelvin lammerts van bueren <kelvinlvb2002@hotmail.com>
2023-03-08 19:35:14 +0100

    Part 4 opdr 22

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,41 +1,13 @@
-﻿using ConsoleApp2;
-
-List<Book> books = new List<Book>();
+﻿List<string> strings = new List<string>();
 
 while (true)
 {
-    Console.Write("Enter book title (or press Enter to finish): ");
-    string title = Console.ReadLine();
-
-    if (string.IsNullOrEmpty(title))
+    string input = Console.ReadLine();
+    if(input == "end")
     {
         break;
     }
-
-    Console.Write("Enter number of pages: ");
-    int pages = int.Parse(Console.ReadLine());
-
-    Console.Write("Enter publication year: ");
-    int publicationYear = int.Parse(Console.ReadLine());
-
-    Book book = new Book(title, pages, publicationYear);
-    books.Add(book);
+    strings.Add(input);
 }
 
-Console.Write("What do you want to print (everything or title)? ");
-string input = Console.ReadLine();
-
-if (input == "everything")
-{
-    foreach (Book book in books)
-    {
-        Console.WriteLine(book.Title+ " (" +book.Pages+ " pages, published in " +book.PublicationYear+ ")");
-    }
-}
-else if (input == "title")
-{
-    foreach (Book book in books)
-    {
-        Console.WriteLine(book.Title);
-    }
-}
+Console.WriteLine(strings.Count);
