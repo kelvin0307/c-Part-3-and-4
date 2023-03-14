97d6e128568718b8d78a3cc26174360da1a31fad
kelvin lammerts van bueren <kelvinlvb2002@hotmail.com>
2023-03-14 19:41:53 +0100

    Part 5 opdr 10

---
+++ ConsoleApp2/Apartment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    internal class Apartment
+    {
+        public int Id;
+        public int Height;
+        public int Price;
+        public Apartment(int id, int height, int price)
+        {
+            Id = id;
+            Height = height;
+            Price = price;
+        }
+
+        public bool LargerThan(Apartment comparred)
+        {
+            if (Height > comparred.Height)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int PriceDifference(Apartment compared)
+        {
+            if (Price > compared.Price)
+            {
+                return Price - compared.Price;
+            }
+            else
+            {
+                return compared.Price - Price;
+            }
+        }
+
+        public bool MoreExpensiveThan(Apartment compared)
+        {
+            if (Price > compared.Price)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,13 +1,7 @@
 ﻿using ConsoleApp2;
 
-Pet lucy = new Pet("Lucy", "golden retriever");
-Person leo = new Person("Leo", lucy);
-Console.WriteLine(leo.Name + "has a friend called" + leo.Pet.Name + "(" + leo.Pet.Breed + ")");
-
-Pet toothless = new Pet("toothless", "dragon");
-Person mike = new Person("Mike", toothless);
-Console.WriteLine(mike.Name + "has a friend called" + mike.Pet.Name + "(" + mike.Pet.Breed + ")");
-
-Pet stitch = new Pet("stitch", "blue alien");
-Person lilo = new Person("lilo", stitch);
-Console.WriteLine(lilo.Name + "has a friend called" + lilo.Pet.Name + "(" + lilo.Pet.Breed + ")");
+Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
+Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
+Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
+Console.WriteLine(manhattanStudioApt.MoreExpensiveThan(atlantaTwoBedroomApt));
+Console.WriteLine(bangorThreeBedroomApt.MoreExpensiveThan(manhattanStudioApt));
