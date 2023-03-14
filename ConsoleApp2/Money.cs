4b55ba0b52d629f8a550d035f1c22d5f945e0a17
kelvin lammerts van bueren <kelvinlvb2002@hotmail.com>
2023-03-14 20:30:04 +0100

    Part 5 opdr 15

---
+++ ConsoleApp2/Money.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Money
+    {
+        private int euros;
+        private int cents;
+
+        public Money(int euros, int cents)
+        {
+            if (cents > 99)
+            {
+                euros = euros + cents / 100;
+                cents = cents % 100;
+            }
+
+            this.euros = euros;
+            this.cents = cents;
+        }
+
+        public Money Plus(Money addition)
+        {
+            int newEuros = euros + addition.euros;
+            int newCents = cents + addition.cents;
+            if (newCents >= 100)
+            {
+                newEuros += newCents / 100;
+                newCents = newCents % 100;
+            }
+
+            return new Money(newEuros, newCents);
+        }
+
+        public Money Minus(Money decreaser)
+        {
+            int newEuros = euros - decreaser.euros;
+            int newCents = cents - decreaser.cents;
+            if (newCents < 0)
+            {
+                newEuros -= 1;
+                newCents += 100;
+            }
+
+            if (newEuros < 0)
+            {
+                newEuros = 0;
+                newCents = 0;
+            }
+
+            return new Money(newEuros, newCents);
+        }
+
+        public bool LessThan(Money compared)
+        {
+            if (euros < compared.euros)
+            {
+                return true;
+            }
+            else if (euros == compared.euros && cents < compared.cents)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            string zero = "";
+            if (cents <= 10)
+            {
+                zero = "0";
+            }
+
+            return euros + "." + zero + cents + "e";
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,17 +1,28 @@
 ﻿using ConsoleApp2;
 
-SimpleDate date = new SimpleDate(13, 2, 2015);
-Console.WriteLine("Friday of the examined week is " + date);
+Money money = new Money(100, 00);
+Money moreMoney = new Money(500, 50);
 
-SimpleDate newDate = date.AfterNumberOfDays(7);
-int week = 1;
-while (week <= 7)
-{
-    Console.WriteLine("Friday after " + week + " weeks is " + newDate);
-    newDate = newDate.AfterNumberOfDays(7);
+Money combined = money.Plus(moreMoney);
 
-    week = week + 1;
-}
+Console.WriteLine(money);         // prints "100.00e"
+Console.WriteLine(moreMoney);    // prints "500.50e"
+Console.WriteLine(combined);     // prints "600.50e"
 
-Console.WriteLine("The date after 790 days from the examined Friday is ... try it out yourself!");
-// Console.WriteLine("Try " + date.AfterNumberOfDays(790));
+Money lessMoney = moreMoney.Minus(money);
+
+Console.WriteLine(money);         // prints "100.00e"
+Console.WriteLine(moreMoney);    // prints "500.50e"
+Console.WriteLine(lessMoney);    // prints "400.50e"
+
+lessMoney = lessMoney.Minus(money);
+
+Console.WriteLine(money);         // prints "100.00e"
+Console.WriteLine(moreMoney);    // prints "500.50e"
+Console.WriteLine(lessMoney);    // prints "300.50e"
+
+Console.WriteLine(lessMoney.LessThan(moreMoney));   // prints "True"
+Console.WriteLine(lessMoney.LessThan(money));       // prints "False"
+
+lessMoney = lessMoney.Minus(moreMoney);
+Console.WriteLine(lessMoney);    // prints "0.00e"
